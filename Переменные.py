273dc0f358ab352c022ed9a2769e76d7a5de14a3
Parrradoks <bobr521@gmail.com>
2024-09-30 01:47:00 +0300

    Update Переменные.py

--- Переменные.py
+++ Переменные.py
@@ -3,3 +3,10 @@ Number_of_hours_spent = 1.5
 Course_name = 'Python'
 Time_per_task = Number_of_hours_spent/Number_of_homework_completed
 print('Курс:',Course_name,', всего задач:',Number_of_homework_completed,', затрачено часов:',Number_of_hours_spent,', среднее время выполнения:',Time_per_task,'часа')
+
+
+a = 12
+b = 1.5
+c = 'Python'
+d = b/a
+print('Курс:',c,', всего задач:',a,', затрачено часов:',b,', среднее время выполнения:',d,'часа')
